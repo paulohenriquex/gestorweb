b03b584560e2d561a037c89100fa44b2860d7c73
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-06-16 12:05:18 -0300

    adicionada a class Usuario

---
+++ gestorweb/src/main/java/br/project/gestorweb/entity/Usuario.java
@@ -0,0 +1,25 @@
+package br.project.gestorweb.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+
+}
