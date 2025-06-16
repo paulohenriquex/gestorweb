f6ded9408cd4ecb82bf412e689e459fe509d840e
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-06-16 12:00:57 -0300

    adicionada a class IngredienteReceita

---
+++ gestorweb/src/main/java/br/project/gestorweb/entity/IngredienteReceita.java
@@ -0,0 +1,29 @@
+package br.project.gestorweb.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class IngredienteReceita {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id")
+    private Produto produto;
+
+    @ManyToOne
+    @JoinColumn(name = "receita_id")
+    private Receita receita;
+}
