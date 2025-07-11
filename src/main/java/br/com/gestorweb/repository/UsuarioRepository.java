cc0fb06a20d3a356271c09df09056ac6f910442e
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-11 17:03:29 -0300

    formatacao do codigo.

--- src/main/java/br/com/gestorweb/repository/UsuarioRepository.java
+++ src/main/java/br/com/gestorweb/repository/UsuarioRepository.java
@@ -1,11 +1,12 @@
 package br.com.gestorweb.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import br.com.gestorweb.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
+    UserDetails findByEmail(String email);
 }
