1418f71f15a01cdc3912f3932b3c5816db867cba
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-11 08:55:09 -0300

    Criacao da classe Fornecedor.java

---
+++ src/main/java/br/com/gestorweb/model/Fornecedor.java
@@ -0,0 +1,41 @@
+package br.com.gestorweb.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "fornecedor")
+public class Fornecedor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nomeFantasia;
+    private String razaoSocial;
+    private String cnpj;
+    private String telefone;
+    private String email;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "endereco_id")
+    private Endereco endereco;
+
+    @ManyToMany(mappedBy = "fornecedores")
+    private Set<Produto> produtos = new HashSet<>();
+}
