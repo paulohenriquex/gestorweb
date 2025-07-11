7fb993a98e16712a3a404e66922318e0986bfa52
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-11 08:56:24 -0300

    Criacao da classe EnderecoRepository.java

---
+++ src/main/java/br/com/gestorweb/repository/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package br.com.gestorweb.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.gestorweb.model.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+
+}
