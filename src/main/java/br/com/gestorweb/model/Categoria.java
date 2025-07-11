033bedbe76cd8fc360d43d6f4afdc64674f387ee
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-11 08:54:09 -0300

    Criacao da classe Categoria.java

---
+++ src/main/java/br/com/gestorweb/model/Categoria.java
@@ -0,0 +1,30 @@
+package br.com.gestorweb.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Produto> produto;
+
+}
