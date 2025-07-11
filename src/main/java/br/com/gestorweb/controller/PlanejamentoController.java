45e1fd2045e8df21d6ace52022f75f4a67ad69a2
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-11 17:03:07 -0300

    removendo as notacoes por enquanto.

--- src/main/java/br/com/gestorweb/controller/PlanejamentoController.java
+++ src/main/java/br/com/gestorweb/controller/PlanejamentoController.java
@@ -1,10 +1,5 @@
 package br.com.gestorweb.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping
 public class PlanejamentoController {
 
 }
