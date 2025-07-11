3cea1d359080d7e56b01c93192d829043d5238a9
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-11 08:55:22 -0300

    Criacao da classe IngredienteReceita.java

---
+++ src/main/java/br/com/gestorweb/model/IngredienteReceita.java
@@ -0,0 +1,34 @@
+package br.com.gestorweb.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ingrediente_receita")
+public class IngredienteReceita {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "receita_id")
+    private Receita receita;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id")
+    private Produto produto;
+
+    private double percapita;
+
+}
