b7388afd1665e93c38859fa2956e3672ce529c3b
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-11 11:29:07 -0300

    Removido a notacao @Repository.

--- src/main/java/br/com/gestorweb/repository/CategoriaRepository.java
+++ src/main/java/br/com/gestorweb/repository/CategoriaRepository.java
@@ -1,11 +1,9 @@
 package br.com.gestorweb.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import br.com.gestorweb.model.Categoria;
 
-@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
 }
