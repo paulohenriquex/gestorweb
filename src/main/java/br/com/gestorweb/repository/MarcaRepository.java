59175d353921091590eba36761607332c503b91f
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-11 11:29:17 -0300

    Removido a notacao @Repository.

--- src/main/java/br/com/gestorweb/repository/MarcaRepository.java
+++ src/main/java/br/com/gestorweb/repository/MarcaRepository.java
@@ -1,11 +1,9 @@
 package br.com.gestorweb.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import br.com.gestorweb.model.Marca;
 
-@Repository
 public interface MarcaRepository extends JpaRepository<Marca, Long> {
 
 }
