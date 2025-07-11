9a68db353edbde7f7ecaf049e59f939bdad0cd24
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-11 16:59:01 -0300

    Implementando  o save e o findAll.

--- src/main/java/br/com/gestorweb/service/ProdutoService.java
+++ src/main/java/br/com/gestorweb/service/ProdutoService.java
@@ -1,8 +1,24 @@
 package br.com.gestorweb.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import br.com.gestorweb.model.Produto;
+import br.com.gestorweb.repository.ProdutoRepository;
 
 @Service
 public class ProdutoService {
-    // TODO
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public ResponseEntity<Produto> save(Produto produto) {
+        return ResponseEntity.ok(produtoRepository.save(produto));
+    }
+
+    public ResponseEntity<List<Produto>> findAll() {
+        return ResponseEntity.ok(produtoRepository.findAll());
+    }
 }
