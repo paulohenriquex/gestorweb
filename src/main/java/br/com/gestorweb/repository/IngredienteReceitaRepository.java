866fd656afb0589cc3f11c86c80d6c2cb7c5bd8b
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-11 08:56:33 -0300

    Criacao da classe IngredienteReceitaRepository.java

---
+++ src/main/java/br/com/gestorweb/repository/IngredienteReceitaRepository.java
@@ -0,0 +1,11 @@
+package br.com.gestorweb.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.gestorweb.model.IngredienteReceita;
+
+@Repository
+public interface IngredienteReceitaRepository extends JpaRepository<IngredienteReceita, Long> {
+
+}
