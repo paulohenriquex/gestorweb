5945392885e6be84c00258078edf734547651525
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-14 10:55:18 -0300

    adicionado <Usuario> findByUsername.

--- src/main/java/br/com/gestorweb/repository/UsuarioRepository.java
+++ src/main/java/br/com/gestorweb/repository/UsuarioRepository.java
@@ -1,12 +1,13 @@
 package br.com.gestorweb.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import br.com.gestorweb.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    UserDetails findByEmail(String email);
+    Optional<Usuario> findByUsername(String username);
 }
