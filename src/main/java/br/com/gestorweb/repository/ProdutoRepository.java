a5435eb1344ba12b15e0f8249ffbd5616c634a51
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-14 10:55:02 -0300

    adicionado List<Produto> findbYUsuarioId.

--- src/main/java/br/com/gestorweb/repository/ProdutoRepository.java
+++ src/main/java/br/com/gestorweb/repository/ProdutoRepository.java
@@ -1,9 +1,11 @@
 package br.com.gestorweb.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.gestorweb.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-
+    List<Produto> findByUsuarioId(Long id);
 }
