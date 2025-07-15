fa8015f6fa37b7adf7eaaa02dbafe819a3426a78
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-15 10:12:49 -0300

    Adicionando o objeto usuario em Endereco com @JoinColumn usuario.

--- src/main/java/br/com/gestorweb/model/Endereco.java
+++ src/main/java/br/com/gestorweb/model/Endereco.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,6 +20,10 @@ public class Endereco {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
 
     private String logradouro;
     private String numero;
