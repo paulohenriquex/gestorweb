4cd3d0fa3fe0889cc5b3207633e98e943e8ccb73
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-15 10:05:53 -0300

    Criacao da classe EnderecoService para salvar, procurar todos os enderecos e procurar endereco por id do usuario.

--- src/main/java/br/com/gestorweb/service/EnderecoService.java
+++ src/main/java/br/com/gestorweb/service/EnderecoService.java
@@ -1,8 +1,31 @@
 package br.com.gestorweb.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.gestorweb.model.Endereco;
+import br.com.gestorweb.repository.EnderecoRepository;
 
 @Service
 public class EnderecoService {
+    @Autowired
+    private final EnderecoRepository enderecoRepository;
 
+    public EnderecoService(EnderecoRepository enderecoRepository) {
+        this.enderecoRepository = enderecoRepository;
+    }
+
+    public Endereco save(Endereco endereco) {
+        return enderecoRepository.save(endereco);
+    }
+
+    public List<Endereco> findAll() {
+        return enderecoRepository.findAll();
+    }
+
+    public List<Endereco> findByUsuarioId(Long usuarioId) {
+        return enderecoRepository.findByUsuarioId(usuarioId);
+    }
 }
