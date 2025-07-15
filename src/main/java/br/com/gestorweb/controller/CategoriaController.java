4e9269372f7c9bd30f6bbe752e59bfa73c9f9af8
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-15 10:09:46 -0300

    Modificacao da classe CategoriaService para salvar e listar todas as categorias.

--- src/main/java/br/com/gestorweb/controller/CategoriaController.java
+++ src/main/java/br/com/gestorweb/controller/CategoriaController.java
@@ -4,25 +4,33 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.gestorweb.model.Categoria;
-import br.com.gestorweb.repository.CategoriaRepository;
+import br.com.gestorweb.model.Usuario;
+import br.com.gestorweb.service.CategoriaService;
 
+@RestController
+@RequestMapping("/api/categoria")
 public class CategoriaController {
 
     @Autowired
-    private CategoriaRepository categoriaRepository;
+    private CategoriaService categoriaService;
 
     @PostMapping
-    public ResponseEntity<Categoria> cadastrarCateforia(@PathVariable Categoria categoria) {
-        return ResponseEntity.ok(categoriaRepository.save(categoria));
+    public ResponseEntity<Categoria> cadastrarCateforia(@RequestBody Categoria categoria,
+            @AuthenticationPrincipal Usuario usuario) {
+        categoria.setUsuario(usuario);
+        return categoriaService.save(categoria);
     }
 
     @GetMapping
     public ResponseEntity<List<Categoria>> listarCategorias() {
-        return ResponseEntity.ok(categoriaRepository.findAll());
+        return categoriaService.findAll();
     }
 }
