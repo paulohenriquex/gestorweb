0cffb755859ebe1ee80103ff18be65f4d585f18e
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-15 10:05:22 -0300

    Criacao da classe FornecedorRepository para salvar, procurar todos os fornecedores e procurar por id.

--- src/main/java/br/com/gestorweb/service/FornecedorService.java
+++ src/main/java/br/com/gestorweb/service/FornecedorService.java
@@ -1,8 +1,32 @@
 package br.com.gestorweb.service;
 
+import java.util.List;
+import br.com.gestorweb.repository.FornecedorRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.gestorweb.model.Fornecedor;
 
 @Service
 public class FornecedorService {
 
+    @Autowired
+    private final FornecedorRepository fornecedorRepository;
+
+    public FornecedorService(FornecedorRepository fornecedorRepository) {
+        this.fornecedorRepository = fornecedorRepository;
+    }
+
+    public Fornecedor save(Fornecedor fornecedor) {
+        return fornecedorRepository.save(fornecedor);
+    }
+
+    public List<Fornecedor> findAll() {
+        return fornecedorRepository.findAll();
+    }
+
+    public List<Fornecedor> findByUsuarioId(Long usuarioId) {
+        return fornecedorRepository.findByUsuarioId(usuarioId);
+    }
 }
