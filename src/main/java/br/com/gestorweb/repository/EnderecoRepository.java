e3d6bf4c1c90c919012741b3328ab74202c5f150
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-15 10:15:12 -0300

    Adicionando List<Endereco> findByUsuarioId em EnderecoRepository.

--- src/main/java/br/com/gestorweb/repository/EnderecoRepository.java
+++ src/main/java/br/com/gestorweb/repository/EnderecoRepository.java
@@ -1,9 +1,11 @@
 package br.com.gestorweb.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.gestorweb.model.Endereco;
 
 public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
-
+    List<Endereco> findByUsuarioId(Long usuarioId);
 }
