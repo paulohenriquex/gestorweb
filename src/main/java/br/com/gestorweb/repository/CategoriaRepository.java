93b1d2da754e0b959143fe7f791ba99b4da2e7d5
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-15 10:14:42 -0300

    Adicionando List<Categoria> findByUsuarioId em CategoriaRepository.

--- src/main/java/br/com/gestorweb/repository/CategoriaRepository.java
+++ src/main/java/br/com/gestorweb/repository/CategoriaRepository.java
@@ -1,9 +1,12 @@
 package br.com.gestorweb.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.gestorweb.model.Categoria;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+    List<Categoria> findByUsuarioId(Long id);
 
 }
