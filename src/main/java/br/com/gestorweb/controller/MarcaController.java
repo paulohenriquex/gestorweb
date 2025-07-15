7331e1487ba08880a48dd9bd18d3181f2c79e687
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-15 10:10:34 -0300

    Modificacao da classe MarcaService, para salvar e listar as todas as marcas.

--- src/main/java/br/com/gestorweb/controller/MarcaController.java
+++ src/main/java/br/com/gestorweb/controller/MarcaController.java
@@ -3,32 +3,35 @@ package br.com.gestorweb.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.gestorweb.model.Marca;
-import br.com.gestorweb.repository.MarcaRepository;
+import br.com.gestorweb.model.Usuario;
+import br.com.gestorweb.service.MarcaService;
 
 @RestController
-@RequestMapping("/marca")
+@RequestMapping("/api/marca")
 public class MarcaController {
 
-    private final MarcaRepository marcaRepository;
+    private final MarcaService marcaService;
 
-    public MarcaController(MarcaRepository marcaRepository) {
-        this.marcaRepository = marcaRepository;
+    public MarcaController(MarcaService marcaService) {
+        this.marcaService = marcaService;
     }
 
     @PostMapping
-    public ResponseEntity<Marca> cadastrarMarca(@PathVariable Marca marca) {
-        return ResponseEntity.ok(marcaRepository.save(marca));
+    public ResponseEntity<Marca> cadastrarMarca(@RequestBody Marca marca, @AuthenticationPrincipal Usuario usuario) {
+        marca.setUsuario(usuario);
+        return marcaService.save(marca);
     }
 
     @GetMapping
     public ResponseEntity<List<Marca>> listarMarcas() {
-        return ResponseEntity.ok(marcaRepository.findAll());
+        return marcaService.findAll();
     }
 }
