f8a6b240568f19f54567e020581d63e673251f46
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-15 10:06:20 -0300

    Modificada a classe UsuarioRepository adicionando UsuarioDetails passando o login.

--- src/main/java/br/com/gestorweb/repository/UsuarioRepository.java
+++ src/main/java/br/com/gestorweb/repository/UsuarioRepository.java
@@ -3,6 +3,7 @@ package br.com.gestorweb.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import br.com.gestorweb.model.Usuario;
@@ -10,4 +11,6 @@ import br.com.gestorweb.model.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByUsername(String username);
+
+    UserDetails findByLogin(String login);
 }
