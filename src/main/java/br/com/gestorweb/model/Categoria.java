9c4d22b33603e4c0bbb0edd888d9d2823a8c3d7c
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-15 10:12:08 -0300

    Adicionada o objeto usuario na Categoria e fazendo um JoinColumn com usuario.

--- src/main/java/br/com/gestorweb/model/Categoria.java
+++ src/main/java/br/com/gestorweb/model/Categoria.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -21,6 +23,10 @@ public class Categoria {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
 
     private String nome;
 
