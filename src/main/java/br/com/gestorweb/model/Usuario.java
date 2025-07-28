3727ad71d4d4f6f62b4fafab03d1d7c91d3ce826
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-28 09:55:55 -0300

    Removido todos os List da classe usuario, nao sao necessários.

--- src/main/java/br/com/gestorweb/model/Usuario.java
+++ src/main/java/br/com/gestorweb/model/Usuario.java
@@ -1,14 +1,9 @@
 package br.com.gestorweb.model;
 
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -29,27 +24,4 @@ public class Usuario {
     private String password;
     private String role;
 
-    @OneToMany(mappedBy = "usuario")
-    @JsonIgnore
-    private List<Produto> produtos;
-
-    @OneToMany(mappedBy = "usuario")
-    @JsonIgnore
-    private List<Marca> marcas;
-
-    @OneToMany(mappedBy = "usuario")
-    @JsonIgnore
-    private List<Fornecedor> fornecedores;
-
-    @OneToMany(mappedBy = "usuario")
-    @JsonIgnore
-    private List<Categoria> categorias;
-
-    @OneToMany(mappedBy = "usuario")
-    @JsonIgnore
-    private List<Receita> receitas;
-
-    @OneToMany(mappedBy = "usuario")
-    @JsonIgnore
-    private List<Planejamento> planejamentos;
 }
