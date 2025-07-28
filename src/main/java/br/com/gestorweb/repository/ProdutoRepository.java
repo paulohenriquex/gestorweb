9358a9eb82e2d983a9658700ef0aef429ec94264
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-28 10:02:48 -0300

    Criada a classe ProdutoRepository

--- src/main/java/br/com/gestorweb/repository/ProdutoRepository.java
+++ src/main/java/br/com/gestorweb/repository/ProdutoRepository.java
@@ -1,9 +1,15 @@
 package br.com.gestorweb.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.gestorweb.model.Produto;
+import br.com.gestorweb.model.Usuario;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
+    List<Produto> findByUsuario(Usuario usuario);
 }
