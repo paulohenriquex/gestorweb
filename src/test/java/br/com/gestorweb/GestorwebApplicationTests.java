75284a12fcb1bfd5755790dba64ac8f37112f991
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-28 08:48:21 -0300

    Criado a classe java GestorwebApplicationTests

---
+++ src/test/java/br/com/gestorweb/GestorwebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gestorweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
