5a074bc64ff39e5044ef75f34501d1c2420b4a94
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-28 09:56:12 -0300

    Adicionado Optional na classe UsuarioRepository.

--- src/main/java/br/com/gestorweb/repository/UsuarioRepository.java
+++ src/main/java/br/com/gestorweb/repository/UsuarioRepository.java
@@ -1,9 +1,12 @@
 package br.com.gestorweb.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.gestorweb.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+    Optional<Usuario> findByEmail(String email);
 }
