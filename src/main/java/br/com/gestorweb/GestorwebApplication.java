e47200a011e64d840526149eac1d4873474444b9
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-28 08:47:08 -0300

    Criado a classe java GestorwebApplication

---
+++ src/main/java/br/com/gestorweb/GestorwebApplication.java
@@ -0,0 +1,13 @@
+package br.com.gestorweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorwebApplication.class, args);
+	}
+
+}
