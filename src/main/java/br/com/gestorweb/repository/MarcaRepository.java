cfcf432a9414c425321ad3f0e08b3a2557fe164d
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-28 10:02:34 -0300

    Criada a classe MarcaRepository

--- src/main/java/br/com/gestorweb/repository/MarcaRepository.java
+++ src/main/java/br/com/gestorweb/repository/MarcaRepository.java
@@ -1,9 +1,14 @@
 package br.com.gestorweb.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.gestorweb.model.Marca;
+import br.com.gestorweb.model.Usuario;
 
+@Repository
 public interface MarcaRepository extends JpaRepository<Marca, Long> {
-
+    List<Marca> findByUsuario(Usuario usuario);
 }
