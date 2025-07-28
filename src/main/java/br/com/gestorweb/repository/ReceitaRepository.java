a6b1f60235b05e52b94259ed1b0ef3b02388e0c7
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-28 10:03:00 -0300

    Criada a classe ReceitaRepository

--- src/main/java/br/com/gestorweb/repository/ReceitaRepository.java
+++ src/main/java/br/com/gestorweb/repository/ReceitaRepository.java
@@ -1,9 +1,14 @@
 package br.com.gestorweb.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.gestorweb.model.Receita;
+import br.com.gestorweb.model.Usuario;
 
+@Repository
 public interface ReceitaRepository extends JpaRepository<Receita, Long> {
-
+    List<Receita> findByUsuario(Usuario usuario);
 }
