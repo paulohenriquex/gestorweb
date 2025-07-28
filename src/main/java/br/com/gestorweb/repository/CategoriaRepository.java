9a1277a2f166d555e0094b5b277574a5cfec9b39
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-28 10:01:23 -0300

    Criada a classe CategoriaRepository

--- src/main/java/br/com/gestorweb/repository/CategoriaRepository.java
+++ src/main/java/br/com/gestorweb/repository/CategoriaRepository.java
@@ -1,9 +1,14 @@
 package br.com.gestorweb.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.gestorweb.model.Categoria;
+import br.com.gestorweb.model.Usuario;
 
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-
+    List<Categoria> findByUsuario(Usuario usuario);
 }
