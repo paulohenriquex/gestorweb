081e76a353c872849b70792a5364ad30ae8f08e9
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-28 08:47:24 -0300

    Criado a classe java Endereco

---
+++ src/main/java/br/com/gestorweb/model/Endereco.java
@@ -0,0 +1,38 @@
+package br.com.gestorweb.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String cep;
+    private String pais;
+
+    @OneToOne(mappedBy = "endereco")
+    @JsonIgnore
+    private Fornecedor fornecedor;
+}
