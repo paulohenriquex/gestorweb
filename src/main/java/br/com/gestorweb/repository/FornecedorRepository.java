34df85c61be22801d5649f91dc62db54f2c52e8f
paulohenriquex <paulohenriquesouzabr@outlook.com>
2025-07-28 10:01:43 -0300

    Criada a classe FornecedorRepository

--- src/main/java/br/com/gestorweb/repository/FornecedorRepository.java
+++ src/main/java/br/com/gestorweb/repository/FornecedorRepository.java
@@ -1,9 +1,17 @@
 package br.com.gestorweb.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.gestorweb.model.Fornecedor;
+import br.com.gestorweb.model.Usuario;
 
+@Repository
 public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
+    List<Fornecedor> findByUsuario(Usuario usuario);
 
+    Optional<Fornecedor> findByCnpj(String cnpj);
 }
